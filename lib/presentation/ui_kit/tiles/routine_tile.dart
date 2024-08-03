6b98b081daebfc119c1a2d6e6103906997a99830
campfire5 <barbaqadzedato3@gmail.com>
2024-08-03 21:00:03 +0400

    feat: routine tile

--- lib/presentation/ui_kit/tiles/routine_tile.dart
+++ lib/presentation/ui_kit/tiles/routine_tile.dart
@@ -17,22 +17,49 @@ class RoutineTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.all(20),
       decoration: BoxDecoration(
         color: UIKitColors.secondaryColor,
-        border: Border.all(
-          width: 2,
-        ),
+        border: Border.all(width: 1, color: UIKitColors.white),
         borderRadius: BorderRadius.circular(20),
       ),
-      child: Row(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
-            name,
-            style: TextStyle(color: UIKitColors.white),
+          Row(
+            children: [
+              Text(
+                name,
+                style: TextStyle(color: UIKitColors.white, fontSize: 16),
+              ),
+              Spacer(),
+              Icon(
+                Icons.timelapse_rounded,
+                color: UIKitColors.white,
+                size: 25,
+              ),
+              SizedBox(width: 4),
+              Text(
+                "${timeMinutes}m",
+                style: TextStyle(color: UIKitColors.white, fontSize: 16),
+              ),
+            ],
           ),
-          Column(
-            children: [],
-          ),
+          const SizedBox(height: 25),
+          Row(
+            children: [
+              Icon(
+                Icons.fitness_center,
+                color: UIKitColors.white,
+                size: 20,
+              ),
+              const SizedBox(width: 4),
+              Text(
+                "${exercises.length} exercises",
+                style: TextStyle(color: UIKitColors.white, fontSize: 14),
+              ),
+            ],
+          )
         ],
       ),
     );
